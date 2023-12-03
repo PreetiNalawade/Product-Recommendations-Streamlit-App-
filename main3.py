25b1cbecdf1d314214e6475adaaee04657ac52d7
PreetiNalawade <preeti.nalawade24@gmail.com>
2023-12-02 21:13:39 -0500

    changes to main

--- main3.py
+++ main3.py
@@ -28,7 +28,7 @@ model = tf.keras.Sequential([
 ])
 
 # Load styles DataFrame
-styles_df = pd.read_csv('styles.csv')
+styles_df = pd.read_csv('styles.csv', error_bad_lines=False)
 
 # Function to recommend products
 def recommend(features, feature_list):
